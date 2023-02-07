eecb30b624f92c04fb04e1e3b001e43b40761151
perqueza72 <practica22correo@gmail.com>
2023-02-06 19:03:52 -0500

    Add constants.

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const HOST_DEV = "http://localhost:8080"
+const HOST_PROD = ""
+
+export const HOST_URL = process.env.NODE_ENV === "production" ? HOST_PROD : HOST_DEV
+export const GET_EMAIL_PAGE_URL = HOST_URL + "/search_records"
