dbfbc1fdf445a5e3b2300c2fb34ef093c25b19d3
perqueza72 <practica22correo@gmail.com>
2023-02-07 16:46:02 -0500

    In table, add sort functions and solve some style errors.

--- src/components/EmailTable.vue
+++ src/components/EmailTable.vue
@@ -5,30 +5,54 @@
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Subject
+                        <div class="flex items-center">
+                            Subject
+                            <a @click="sortEmailsBy('Subject')" class="cursor-pointer"><svg
+                                    xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true"
+                                    fill="currentColor" viewBox="0 0 320 512">
+                                    <path
+                                        d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                </svg></a>
+                        </div>
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        From
+                        <div class="flex items-center">
+                            From
+                            <a @click="sortEmailsBy('From')" class="cursor-pointer"><svg
+                                    xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true"
+                                    fill="currentColor" viewBox="0 0 320 512">
+                                    <path
+                                        d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                </svg></a>
+                        </div>
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        To
+                        <div class="flex items-center">
+                            To
+                            <a @click="sortEmailsBy('To')" class="cursor-pointer"><svg
+                                    xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true"
+                                    fill="currentColor" viewBox="0 0 320 512">
+                                    <path
+                                        d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                </svg></a>
+                        </div>
                     </th>
                 </tr>
             </thead>
             <tbody>
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="email in emails"
-                    :key="email.id" @click="selectEmail(email)"
-                    
-                    >
-                    <th scope="row" class="overflow-hidden max-w-sm px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    :key="email.id" @click="selectEmail(email)">
+                    <th scope="row"
+                        class="overflow-hidden max-w-sm px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ email.Subject }}
                     </th>
                     <td class="px-6 py-4">
                         {{ email.From }}
                     </td>
-                    <td class="px-6 py-4 overflow-y-scroll">
-
-                        {{ email.To }}
+                    <td class="px-6 py-4">
+                        <p class="overflow-y-scroll max_size">
+                            {{email.To }}
+                        </p>
                     </td>
                 </tr>
             </tbody>
@@ -42,7 +66,14 @@ export default {
     name: "EmailTable",
     props: {
         emails: Array,
-        selectEmail: Function
+        selectEmail: Function,
+        sortEmailsBy: Function
     }
 }
 </script>
+
+<style>
+.max_size{
+    max-height: 80px;
+}
+</style>
