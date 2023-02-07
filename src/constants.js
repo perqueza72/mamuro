2f9c71ff3fc71939601c493bf7315dcedb482d91
perqueza72 <practica22correo@gmail.com>
2023-02-07 16:44:35 -0500

    Add new contant in Urls.

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ const HOST_PROD = ""
 
 export const HOST_URL = process.env.NODE_ENV === "production" ? HOST_PROD : HOST_DEV
 export const GET_EMAIL_PAGE_URL = HOST_URL + "/search_records"
+export const GET_EMAIL_PAGE_BY_SEARCH_URL = HOST_URL + "/find_records"
